571f1a6324890006498a7a054b68bd20336feb81
Dadflip <92120636+dadflip@users.noreply.github.com>
2024-04-21 20:34:35 +0200

    Delete flipapp directory

--- flipapp/.base.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Définir l'include path avec un chemin absolu
-set_include_path(get_include_path() . PATH_SEPARATOR . 'C:\xampp\htdocs\flipapp\_assets\php');
-
-// Inclure le fichier '_database_include.php'
-include('x_flipapp_include_db.php');
-include('x_flipapp_include_vars.php');
-?>
